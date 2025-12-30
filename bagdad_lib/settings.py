aa1d73a1dba4cac82110f88ed817ade4d9b83be4
GoldenDrag <mr.ablaysan@gmail.com>
2025-12-30 21:21:52 +0500

    renaming bd

--- bagdad_lib/settings.py
+++ bagdad_lib/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'bagdad_lib.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        "NAME": "badgad_lib",
+        "NAME": "bagdad_lib",
         "USER": "postgres",
         "PASSWORD": "postgres",
         "HOST": "127.0.0.1",
